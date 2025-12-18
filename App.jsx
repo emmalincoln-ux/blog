7b01aa407769fe3b7b2797f6cb32cfcd01d5e3c8
emmalincoln-ux <emma.lincoln@cinesend.com>
2025-12-18 13:20:57 -0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,10 +1,20 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 
+function Home() {
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">
+        Cinesend Beta UI
+      </h1>
+    </div>
+  );
+}
+
 function App() {
   return (
     <HashRouter>
       <Routes>
-        {/* your routes */}
+        <Route path="/" element={<Home />} />
       </Routes>
     </HashRouter>
   );
